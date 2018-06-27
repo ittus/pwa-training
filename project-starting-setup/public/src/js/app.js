617d8f45777482f41d909b6f67efcfe8989c399c
ittus <vuminhthang.cm@gmail.com>
2018-06-27 22:56:46 +0900

    Add assets

--- project-starting-setup/public/src/js/app.js
+++ project-starting-setup/public/src/js/app.js
@@ -1,14 +1,17 @@
 var deferredPrompt;
 
 if (!window.Promise) {
-    window.Promise = Promise;
+  window.Promise = Promise;
 }
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
     .register('/sw.js')
-    .then(function() {
-      console.log('Service worker registered!')
+    .then(function () {
+      console.log('Service worker registered!');
+    })
+    .catch(function(err) {
+      console.log(err);
     });
 }
 
