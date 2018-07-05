616c68af85bac4b873bb290bd323cf087bd50331
ittus <vuminhthang.cm@gmail.com>
2018-07-06 00:26:06 +0900

    Add caches version

--- project-starting-setup/public/sw.js
+++ project-starting-setup/public/sw.js
@@ -1,8 +1,10 @@
+var CACHE_STATIC_NAME = 'static-v4'
+var CACHE_DYNAMIC_NAME = 'dynamic-v2'
 
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
   event.waitUntil(
-    caches.open('static')
+    caches.open(CACHE_STATIC_NAME)
       .then(function(cache) {
         console.log('[Service Worker] Precaching App Shell');
         cache.addAll([
@@ -26,6 +28,17 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('activate', function(event) {
   console.log('[Service Worker] Activating Service Worker ....', event);
+  event.waitUntil(
+    caches.keys()
+      .then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+            console.log('[Service Worker] Removing old cache.', key);
+            return caches.delete(key);
+          }
+        }))
+      })
+  )
   return self.clients.claim();
 });
 
@@ -38,12 +51,13 @@ self.addEventListener('fetch', function(event) {
         } else {
           return fetch(event.request)
             .then(function(res) {
-              return caches.open('dynamic')
+              return caches.open(CACHE_DYNAMIC_NAME)
                 .then(function(cache) {
                   cache.put(event.request.url, res.clone());
                   return res;
                 })
-            });
+            })
+            .catch(() => {});
         }
       })
   );
