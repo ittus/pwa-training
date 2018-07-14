cbf92696e1dba9c818dfd2d259aa876b801c791f
ittus <vuminhthang.cm@gmail.com>
2018-07-14 16:33:25 +0900

    Cache-only strategy

--- project-starting-setup/public/sw.js
+++ project-starting-setup/public/sw.js
@@ -43,28 +43,36 @@ self.addEventListener('activate', function(event) {
   return self.clients.claim();
 });
 
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(function(response) {
+//         if (response) {
+//           return response;
+//         } else {
+//           return fetch(event.request)
+//             .then(function(res) {
+//               return caches.open(CACHE_DYNAMIC_NAME)
+//                 .then(function(cache) {
+//                   cache.put(event.request.url, res.clone());
+//                   return res;
+//                 })
+//             })
+//             .catch(() => {
+//               return caches.open(CACHE_STATIC_NAME)
+//                 .then(function(cache) {
+//                   return cache.match('/offline.html')
+//                 })
+//             });
+//         }
+//       })
+//   );
+// });
+
+
+// Cache-only
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
-        } else {
-          return fetch(event.request)
-            .then(function(res) {
-              return caches.open(CACHE_DYNAMIC_NAME)
-                .then(function(cache) {
-                  cache.put(event.request.url, res.clone());
-                  return res;
-                })
-            })
-            .catch(() => {
-              return caches.open(CACHE_STATIC_NAME)
-                .then(function(cache) {
-                  return cache.match('/offline.html')
-                })
-            });
-        }
-      })
   );
 });
