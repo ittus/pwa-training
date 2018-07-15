cc080643ed0e3dc8a0931cc9978b4677bb65e172
ittus <vuminhthang.cm@gmail.com>
2018-07-15 15:51:00 +0900

    Change css

--- project-starting-setup/public/src/js/feed.js
+++ project-starting-setup/public/src/js/feed.js
@@ -5,6 +5,10 @@ var sharedMomentsArea = document.querySelector('#shared-moments');
 
 function openCreatePostModal() {
   createPostArea.style.display = 'block';
+  setTimeout(function() {
+    createPostArea.style.transform = 'translateY(0)';
+  }, 1)
+
   if (deferredPrompt) {
     deferredPrompt.prompt();
 
@@ -31,7 +35,8 @@ function openCreatePostModal() {
 }
 
 function closeCreatePostModal() {
-  createPostArea.style.display = 'none';
+  createPostArea.style.transform = 'translateY(100vh)';
+  // createPostArea.style.display = 'none';
 }
 
 shareImageButton.addEventListener('click', openCreatePostModal);
@@ -61,7 +66,6 @@ function createCard(data) {
   cardTitle.className = 'mdl-card__title';
   cardTitle.style.backgroundImage = 'url("' + data.image + '")';
   cardTitle.style.backgroundSize = 'cover';
-  cardTitle.style.height = '180px';
   cardWrapper.appendChild(cardTitle);
   var cardTitleTextElement = document.createElement('h2');
   cardTitleTextElement.style.color = 'white';
--- project-starting-setup/public/sw.js
+++ project-starting-setup/public/sw.js
@@ -1,7 +1,7 @@
 importScripts('/src/js/idb.js');
 importScripts('/src/js/utility.js');
 
-var CACHE_STATIC_NAME = 'static-v21'
+var CACHE_STATIC_NAME = 'static-v22'
 var CACHE_DYNAMIC_NAME = 'dynamic-v2'
 
 var STATIC_FILES =[
